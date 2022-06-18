cac4a89910e5215d893df4a1a173574d73e8040e
Kalaivanikalai <97941263+Kalaivanikalai@users.noreply.github.com>
2022-06-18 22:34:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,27 +10,35 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("DB Connetion Successfull");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
-
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const server = app.listen(process.env.PORT, () =>
-  console.log(`Server started on ${process.env.PORT}`)
+
+
+app.get('/',(req,res)=>{
+  res.send("App is Running");
+})
+
+MONGO_URL="mongodb+srv://chatapp:chatapp123@cluster0.8jwgb.mongodb.net/chatapp?retryWrites=true&w=majority"
+
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+.then(()=>{console.log("Database connected")})
+.catch((error)=>{console.log(error);})
+
+const PORT=process.env.PORT || 5000;
+
+const server = app.listen(PORT, () =>{
+  console.log(`Server started on ${PORT}`)
+}
 );
+
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     credentials: true,
   },
 });
